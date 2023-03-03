be7bf1f26cb592ac14a7855ff2937667e1d87c20
Sdby2003 <115440321+Sdby2003@users.noreply.github.com>
2023-03-03 13:19:23 -0600

    models data types added

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,14 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    product_id: {
+      type: DataType.INTENGER,
+      allowNull: false,
+    },
+    tag_id: {
+      type: DataType.INTENGER,
+      allowNull: false,
+    }
   },
   {
     sequelize,
