f18f55731e68472c9a5ebeaee3589f83e4924544
Sdby2003 <115440321+Sdby2003@users.noreply.github.com>
2023-03-03 15:52:15 -0600

    all get routes succesfull

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,11 +4,8 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', async (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
-      // Add Book as a second model to JOIN with
       include: { model: Product}
     });
     res.status(200).json(categoryData);
@@ -17,9 +14,21 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: { model:Product },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
